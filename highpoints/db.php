191852020bc43a111e2009d1b82dbaeae91cccae
justinandersun <justinandersun@gmail.com>
2014-03-17 02:43:27 -0400

    Removed unsecure data

--- highpoints/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;dbname=si364','justin','tree');
-// See the "errors" folder for details...
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
