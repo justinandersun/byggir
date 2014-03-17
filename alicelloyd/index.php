a5307936cdd1059f0efeb796cea1a668b2e4ecc2
justinandersun <justinandersun@gmail.com>
2014-03-17 02:37:38 -0400

    Added even MORE files

---
+++ alicelloyd/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("head.php") ?>
+<body>
+	<?php include("header.php") ?>
+	<h3>Welcome to Alice Lloyd!</h3>
+	<?php include("footer.php") ?>
+</body>
+</html>
---
+++ alicelloyd/students.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("head.php") ?>
+<body>
+	<?php include("header.php") ?>
+	<h3>STUDENTS</h3>
+	<?php include("footer.php") ?>
+</body>
+</html>
