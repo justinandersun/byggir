1900dc87accf52ec4c224e05bdabb061bce5ab9f
justinandersun <justinandersun@gmail.com>
2014-03-17 02:38:26 -0400

    Now this is just getting ridiculous

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("head.php") ?>
+<body>
+	<h1>BYGGIR</h1>
+	<p class="description">Some projects under construction</p>
+	<br>
+	<br>
+	<a href="highpoints/index.php">Highpoints</a>
+	<a href="alicelloyd/index.php">Alice Lloyd</a>
+</body>
+</html>
